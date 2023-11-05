71a1962c1c55b4ddc253423a684d70840410b81d
Moca <mocaminecraft999@gmail.com>
2023-11-05 17:08:47 -0600

    modified: final/settings.py

--- final/settings.py
+++ final/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'final.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
